c2c7fd4e1ec52940d211771c667d7192e406102e
Victor Borja <vborja@apache.org>
2016-12-27 13:11:12 -0600

    fix minibuffer prompt

--- rebecca-theme.el
+++ rebecca-theme.el
@@ -101,7 +101,7 @@
    `(link ((,class (:foreground ,const :underline t))))
    `(linum ((,class (:foreground ,comment :background ,bg1 :height 0.9))))
    `(linum-relative-current ((,class (:foreground ,fg1 :background ,bg1 :height 0.9))))
-   `(minibuffer-prompt ((,class (:bold t :foreground ,str))))
+   `(minibuffer-prompt ((,class (:bold t :foreground ,func))))
    `(region ((,class (:background ,rebecca))))
    `(show-paren-match-face ((,class (:background ,warning))))
    `(tooltip ((,class (:foreground ,fg2 :background ,bg0))))
